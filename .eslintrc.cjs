6586154536eab2f85ff335e59d3692afe93f4abd
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-08-22 19:06:57 +0530

    configure eslint and prettier for react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,21 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:jsx-a11y/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react', 'import', 'jsx-a11y'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 0,
+    'indent': ['error', 2],
+    'linebreak-style': 1,
+    'quotes': ['error', 'double'],
   },
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-// import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    
+    <Router>
 
-    </div>
+      <Routes>
+
+        <Route path="/" />
+
+      </Routes>
+
+    </Router>
   )
 }
 
